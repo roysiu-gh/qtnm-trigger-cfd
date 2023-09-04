dfa705588478bb954163889abee70388264dec8a
Roy Siu <roysoup@outlook.com>
2023-09-04 22:40:34 +0100

    Move actual plotting to only when InteractiveTrigger.show() is called. Make changing view range faster (logical error).

--- analysis/interactive_trigger_class.py
+++ analysis/interactive_trigger_class.py
@@ -28,47 +28,12 @@ class InteractiveTrigger(SignalData):
             *args,
             **kwargs,
         )
+        self.title = title
+        self.yscale = yscale
+
         view_range_min = self.all_t[self.slice_start]
         view_range_max = self.all_t[self.slice_end]  # NB [-1] indexing does not work with pandas dataframe
         self.view_range = [view_range_min, view_range_max]
-
-        self.fig, self.axis = plt.subplots(figsize=FIGSIZE)
-        self.fig.suptitle(title)
-
-        self.axis.set_xlim(self.view_range)
-        self.axis.set_yscale(yscale)
-        self.axis.plot(self.t, self.sig_amp, label="Amplifier output")
-        self.axis.plot(self.t, self.sig_fil1, label="Filter output")
-        self.plt_cfd1, = self.axis.plot(self.t, self.sig_cfd1, label="CFD output 1")
-        self.plt_fil2, = self.axis.plot(self.t, self.sig_cfd2, label="LP filt 2")
-        self.plt_cfd2, = self.axis.plot(self.t, self.sig_cfd2, label="CFD output 2")
-        self.plt_zer = self.axis.scatter(*self.get_nonzeros(self.output))
-        self.plt_tru = self.axis.scatter(*self.get_nonzeros(self.truth_data))
-
-        self.hitrate_text = Label()
-        self.misfire_rate_text = Label()
-
-        # Show triggers and truth data with graphics settings as in update(). Works by... logic.
-        self.update(delay_samples=None, inv_frac=None, view_range=None)
-        # self.update(delay_samples=self.delay_samples, inv_frac=self.inv_frac, view_range=self.view_range)
-
-        delay_samples_slider = IntSlider(min=0, max=300, step=10,
-                                         value=self.delay_samples, description="Delay samples",
-                                         layout=Layout(width="50%"), )
-        inv_frac_slider = FloatSlider(min=0, max=6, step=0.1,
-                                      value=self.inv_frac, description="Inverse fraction",
-                                      layout=Layout(width="50%"), )
-        view_range_slider = FloatRangeSlider(min=0, max=view_range_max, step=10e-6,
-                                             value=[0, view_range_max], description="View range",
-                                             layout=Layout(width="95%"),
-                                             )
-
-        self.widget = interactive(self.update,
-                                  delay_samples=delay_samples_slider,
-                                  inv_frac=inv_frac_slider,
-                                  view_range=view_range_slider,
-                                  continuous_update=False,
-                                  )
 
     def get_nonzeros(self, series):
         """Returns datapoints where 'y'-val is non-zero."""
@@ -81,14 +46,13 @@ class InteractiveTrigger(SignalData):
         return times, nonzerodata
 
     def update(self, delay_samples=None, inv_frac=None, view_range=None):
-        if self.view_range != view_range:
-            self.view_range = view_range or self.view_range
-            self.axis.set_xlim(view_range)
+        if view_range is not None:
+            self.view_range = view_range
+            self.axis.set_xlim(self.view_range)
 
-        if (self.inv_frac != inv_frac) or (self.delay_samples != delay_samples):
-            # Only update if not none:
-            self.inv_frac = inv_frac or self.inv_frac
-            self.delay_samples = delay_samples or self.delay_samples
+        if (inv_frac is not None) or (delay_samples is not None):
+            self.inv_frac = inv_frac
+            self.delay_samples = delay_samples
 
             self.run_cfd1()
             self.run_fil2()
@@ -96,10 +60,8 @@ class InteractiveTrigger(SignalData):
             self.run_zd()
 
             test_parameters = self.get_current_performance()
-            hitrate = test_parameters["hitrate"]
-            misfire_rate = test_parameters["misfire_rate"]
-            self.hitrate_text.value = f"Hitrate: {hitrate}"
-            self.misfire_rate_text.value = f"Misfire rate: {misfire_rate}"
+            self.hitrate_text.value = f"Hitrate: { test_parameters['hitrate'] }"
+            self.misfire_rate_text.value = f"Misfire rate: { test_parameters['misfire_rate'] }"
 
             self.plt_cfd1.set_ydata(self.sig_cfd1)
             self.plt_fil2.set_ydata(self.sig_fil2)
@@ -115,4 +77,40 @@ class InteractiveTrigger(SignalData):
         plt.legend(loc="upper right")
 
     def show(self):
+        self.fig, self.axis = plt.subplots(figsize=FIGSIZE)
+        self.fig.suptitle(self.title)
+
+        self.axis.set_xlim(self.view_range)
+        self.axis.set_yscale(self.yscale)
+        self.axis.plot(self.t, self.sig_amp, label="Amplifier output")
+        self.axis.plot(self.t, self.sig_fil1, label="Filter output")
+        self.plt_cfd1, = self.axis.plot(self.t, self.sig_cfd1, label="CFD output 1")
+        self.plt_fil2, = self.axis.plot(self.t, self.sig_cfd2, label="LP filt 2")
+        self.plt_cfd2, = self.axis.plot(self.t, self.sig_cfd2, label="CFD output 2")
+        self.plt_zer = self.axis.scatter(*self.get_nonzeros(self.output))
+        self.plt_tru = self.axis.scatter(*self.get_nonzeros(self.truth_data))
+
+        self.hitrate_text = Label()
+        self.misfire_rate_text = Label()
+
+        delay_samples_slider = IntSlider(min=0, max=300, step=10,
+                                         value=self.delay_samples, description="Delay samples",
+                                         layout=Layout(width="50%"), )
+        inv_frac_slider = FloatSlider(min=0, max=6, step=0.5,
+                                      value=self.inv_frac, description="Inverse fraction",
+                                      layout=Layout(width="50%"), )
+        view_range_slider = FloatRangeSlider(min=self.view_range[0], max=self.view_range[1], step=10e-6,
+                                             value=self.view_range, description="View range",
+                                             layout=Layout(width="95%"),
+                                             )
+
+        self.widget = interactive(self.update,
+                                  delay_samples=delay_samples_slider,
+                                  inv_frac=inv_frac_slider,
+                                  view_range=view_range_slider,
+                                  continuous_update=False,
+                                  )
+
+        # Show triggers and truth data with graphics settings as in update(). Works by... logic.
+        self.update(delay_samples=None, inv_frac=None, view_range=None)
         display(self.hitrate_text, self.misfire_rate_text, self.widget)
