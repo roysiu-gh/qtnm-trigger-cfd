e2e28368185be41dcc260683c1a4f8a0d79b6504
Roy Siu <roysoup@outlook.com>
2023-09-12 22:16:12 +0100

    Tidy constants file.

--- analysis/default_constants.py
+++ analysis/default_constants.py
@@ -1,16 +1,18 @@
+# Filter params
 DECAY_PART = 900
 WINDOW_WIDTH = 100
 ALPHA = 0.05
-DELAY = 0.2
-FRACTION = 0.2
-SNR = 3  # Signal-to-noise ratio
-TOLERANCE = 100e-6
 
-SLICE_START = 0
-SLICE_END = 10000
+# CFD params
 DELAY_SAMPLES = 100
 INV_FRAC = 3
 
-FIGSIZE = (13, 5)
-
+# ROC curve generation
+TOLERANCE = 100e-6
 SECTION_TIME = 2e-3
+
+# Misc
+SNR = 3  # Signal-to-noise ratio
+SLICE_START = 0
+SLICE_END = 10000
+FIGSIZE = (13, 5)
